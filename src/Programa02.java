49933f4c4ef42b683d5bf291e523a8cc5c8694fc
Giovana Victória <giovanavictoria887@gmail.com>
2021-08-21 09:37:50 -0300

    tres

--- src/Programa02.java
+++ src/Programa02.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class Programa02 {
+// Seção 3 - Aula 12: O compilador e o bytecode
+public class Programa02 {
+    public static void main(String[] args) {
+        // Declarando e inicializando
+        int idade = 28;
+        System.out.println("Eu tenho " + idade + " anos");
+    }
 }
--- src/Programa03.java
+++ src/Programa03.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class Programa03 {
+// A função main
+
+// Linguagem orientada a objetos
+public class Programa03 {
+    // Essa função que torna a classe java em um programa java
+    public static void main(String[] args) {
+        System.out.println("Qualquer texto");
+    }
 }
