3aaffd4e65804714e12e7a1e47c53c49283f967c
Giovana Victória <giovanavictoria887@gmail.com>
2021-08-22 10:03:34 -0300

    cinco

---
+++ src/Programa07.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+// Seção 5 - Aula 21: While e do while
+public class Programa07 {
+    public static void main(String[] args) {
+        int idade = 1;
+        String nome;
+
+        // Para receber entrada do usuário
+        Scanner teclado = new Scanner(System.in);
+
+        // Sempre verifica o valor antes de executar o bloco
+        while(idade > 0) {
+            System.out.println("Informe seu nome:");
+            // Para receber uma entrada tipo string
+            nome = teclado.nextLine();
+
+            System.out.println("Informe sua idade:");
+            // Para receber uma entrada tipo int
+            // idade = teclado.nextInt(); // Bug
+            // Recebe o dado como str e converte em int
+            idade = Integer.parseInt(teclado.nextLine());
+
+            System.out.println(nome + " tem " + idade + " anos");
+        }
+
+        teclado.close();
+
+    }
+}
---
+++ src/Programa08.java
@@ -0,0 +1,33 @@
+// Seção 5 - Aula 21 parte 2: While e do while
+
+import java.util.Scanner;
+
+public class Programa08 {
+    public static void main(String[] args) {
+        int idade;
+        String nome;
+
+        // Para receber entrada do usuário
+        Scanner teclado = new Scanner(System.in);
+
+        // Primeiro executar o bloco, depos faz a checagem
+        do {
+            System.out.println("Informe seu nome:");
+            // Para receber uma entrada tipo string
+            nome = teclado.nextLine();
+
+            System.out.println("Informe sua idade:");
+            // Para receber uma entrada tipo int
+            // idade = teclado.nextInt(); // Bug
+            // Recebe o dado como str e converte em int
+            idade = Integer.parseInt(teclado.nextLine());
+
+            if(idade >0) {
+                System.out.println(nome + " tem " + idade + " anos");
+            }
+        }while(idade > 0);
+
+        teclado.close();
+
+    }
+}
